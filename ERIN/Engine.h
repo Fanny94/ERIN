bc1394e4ce8447f3029ee1da6f1f02f4a61cd573
Taccoa <Mattias_M_93@hotmail.com>
2016-04-08 15:05:33 +0200

    Move code from Engine to Graphics

--- ERIN/Engine.h
+++ ERIN/Engine.h
@@ -10,47 +10,9 @@ public:
 	Engine();
 	~Engine();
 
-	struct TriangleVertex
-	{
-		float x, y, z;
-		float r, g, b;
-	};
-
-	struct MATRICES
-	{
-		Matrix worldViewProj;
-		Matrix world;
-		Matrix view;
-		Matrix projection;
-	};
-
-	TriangleVertex* triangleVertices;
-	const float WIDTH = 640;
-	const float HEIGHT = 480;
-
-	void Render();
-	void SetViewport();
 	int Run(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCommandLine, int nCommandShow);
 
-	IDXGISwapChain* gSwapChain = nullptr;
-	ID3D11Device* gDevice = nullptr;
-	ID3D11DeviceContext* gDeviceContext = nullptr;
-	ID3D11RenderTargetView* gBackbufferRTV = nullptr;
-
 	HWND InitWindow(HINSTANCE hInstance);
-	HRESULT CreateDirect3DContext(HWND wndHandle);
-
-	ID3D11InputLayout* gVertexLayout = nullptr;
-	ID3D11VertexShader* gVertexShader = nullptr;
-	ID3D11Buffer* gVertexBuffer = nullptr;
-	ID3D11PixelShader* gPixelShader = nullptr;
-
-	ID3D11Buffer* gConstantBuffer = nullptr;
-
-	void CreateShaders();
-	void CreateTriangle();
-	void CreateConstantBuffer();
-	void UpdateConstantBuffer();
 };
 
 #endif // !ENGINE_H
