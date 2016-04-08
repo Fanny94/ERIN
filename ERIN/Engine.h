634c16f036c87179f7ec6fb2d33f8ef5d8034f96
Taccoa <Mattias_M_93@hotmail.com>
2016-04-08 10:57:38 +0200

    Moved Window to Engine

--- ERIN/Engine.h
+++ ERIN/Engine.h
@@ -1,8 +1,21 @@
 #pragma once
+#include "Linker.h"
+
 class Engine
 {
 public:
 	Engine();
 	~Engine();
-};
 
+	void Render();
+	void SetViewport();
+	int Run(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCommandLine, int nCommandShow);
+
+	IDXGISwapChain* gSwapChain = nullptr;
+	ID3D11Device* gDevice = nullptr;
+	ID3D11DeviceContext* gDeviceContext = nullptr;
+	ID3D11RenderTargetView* gBackbufferRTV = nullptr;
+
+	HWND InitWindow(HINSTANCE hInstance);
+	HRESULT CreateDirect3DContext(HWND wndHandle);
+};
---
+++ ERIN/Linker.h
@@ -0,0 +1,14 @@
+#ifndef LINKER_H
+#define LINKER_H
+
+#include <Windows.h>
+
+#include <d3d11.h>
+#include <d3dcompiler.h>
+
+#pragma comment (lib, "d3d11.lib")
+#pragma comment (lib, "d3dcompiler.lib")
+#pragma comment (lib, "dinput8.lib")
+#pragma comment (lib, "dxguid.lib")
+
+#endif
