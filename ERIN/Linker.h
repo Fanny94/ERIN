4fd0405134a4820f2a12025baeb8f244ea287d0c
Taccoa <Mattias_M_93@hotmail.com>
2016-04-08 11:15:04 +0200

    Expanded Linker.h

--- ERIN/Linker.h
+++ ERIN/Linker.h
@@ -2,9 +2,11 @@
 #define LINKER_H
 
 #include <Windows.h>
-
 #include <d3d11.h>
 #include <d3dcompiler.h>
+#include <math.h>
+#include <vector>
+#include <DirectXMath.h>
 
 #pragma comment (lib, "d3d11.lib")
 #pragma comment (lib, "d3dcompiler.lib")
