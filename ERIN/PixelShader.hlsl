a777ac7ef78d9def3d07ba0b11953dc98a3e3c75
Taccoa <Mattias_M_93@hotmail.com>
2016-04-15 16:08:29 +0200

    Basic Lighting

--- ERIN/PixelShader.hlsl
+++ ERIN/PixelShader.hlsl
@@ -1,5 +1,5 @@
 SamplerState ObjSamplerState;
-Texture2D ObjTexture;
+Texture2D ObjTexture: register(t0);
 
 cbuffer OBJ
 {
@@ -18,12 +18,23 @@ struct VS_OUT
 
 float4 PS_main(VS_OUT input) : SV_Target
 {
+	float4 lightPosition = float4(0.0, 0.0, 10.0, 0.0);
+	float3 lightIntensity = float3(1.0, 1.0, 1.0);
+
 	input.Nor = normalize(input.Nor);
-	
-	float4 color = difColor;
+
+	float4 s = normalize(lightPosition - input.WPos);
+
+	float3 diffuse = difColor.xyz;
 
 	if (hasTexture == true)
-		color = ObjTexture.Sample(ObjSamplerState, input.uv);
+		diffuse = ObjTexture.Sample(ObjSamplerState, input.uv).xyz;
+
+	float3 diffuseLight = diffuse * max(dot(s, input.Nor), 0.0f);
+
+	float3 ambient = float3(0.2f, 0.2f, 0.2f);
+
+	float4 color = float4((lightIntensity * (ambient + diffuseLight)), 1.0f);
 
 	return color;
 };
