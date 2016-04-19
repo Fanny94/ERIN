91d0bc18c63365a415d7c65604586f0cbad56577
Taccoa <Mattias_M_93@hotmail.com>
2016-04-19 11:53:52 +0200

    Handle Binary

--- ERIN/CustomImport.cpp
+++ ERIN/CustomImport.cpp
@@ -6,11 +6,24 @@ CustomImport::~CustomImport(){}
 
 void CustomImport::LoadCustomFormat(string filePath)
 {
-	ifstream fileIn(filePath);
+	ifstream fileIn(filePath, ios::in | ios::binary);
+	
+	fileIn.read((char*)&MeshCount, sizeof(unsigned int));
+	fileIn.read((char*)&MaterialCount, sizeof(unsigned int));
 
-	if (fileIn)
+	for (int i = 0; i < MeshCount; i++)
 	{
-		while (fileIn)
+		fileIn.read((char*)&VertexCount, sizeof(unsigned int));
+		fileIn.read((char*)&MaterialID, sizeof(unsigned int));
+		fileIn.read((char*)&MeshID, sizeof(unsigned int));
+		fileIn.read((char*)&ParentID, sizeof(unsigned int));
+		fileIn.read((char*)&MeshName, sizeof(char) * 256);
+		fileIn.read((char*)&AttributeCount, sizeof(unsigned int));
+		fileIn.read((char*)&Translation, sizeof(float) * 3);
+		fileIn.read((char*)&Roatation, sizeof(float) * 3);
+		fileIn.read((char*)&Scale, sizeof(float) * 3);
+
+		for (int j = 0; j < VertexCount; j++)
 		{
 
 		}
--- ERIN/CustomImport.h
+++ ERIN/CustomImport.h
@@ -7,9 +7,16 @@ class CustomImport
 {
 private:
 	unsigned int MeshCount;
+	unsigned int MaterialCount;
 	unsigned int VertexCount;
 	unsigned int MaterialID;
 	unsigned int MeshID;
+	unsigned int ParentID;
+	unsigned int AttributeCount;
+	char MeshName[256];
+	float Translation[3];
+	float Roatation[3];
+	float Scale[3];
 public:
 	CustomImport();
 	~CustomImport();
