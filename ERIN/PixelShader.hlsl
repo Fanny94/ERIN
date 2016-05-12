91a480750caf777eb2aec0c31d7588369a02fe48
Fanny94 <funnei@hotmail.com>
2016-05-12 11:04:43 +0200

    light test

--- ERIN/PixelShader.hlsl
+++ ERIN/PixelShader.hlsl
@@ -27,26 +27,28 @@ float4 PS_main(VS_OUT input) : SV_Target
 {
 	float3 diffuseLight;
 	float4 lightPosition = float4(-60.0, 0.0, -60.0, 0.0);
-	float3 lightIntensity = float3(0.9, 0.9, 0.9);
+	float3 lightIntensity = { 0.9, 0.9, 0.9 };
 
-	float4 s = normalize(lightPosition - input.WPos);
+	float4 lightDir = normalize(lightPosition - input.WPos);
 	float4 v = normalize(CamPos - input.WPos);
-	float4 r = reflect(-s, input.Nor);
+	//float4 r = reflect(-s, input.Nor);
 
 	float3 diffuseMap = CustomTexture.Sample(CustomSamplerState, input.uv).xyz;
-
+	
 	input.Nor = normalize(input.Nor);
+	float3 ambientLight = { 0.2, 0.2, 0.2 };
+
+	float diffuseCalc = max(dot(lightDir, input.Nor.xyz), 0.0f);
+
 	if(textureBool == true)
-		diffuseLight = diffuseMap * diffuseColor * max(dot(s, input.Nor), 0.0f);
+		diffuseLight = diffuseMap /** diffuseColor * max(dot(lightDir, input.Nor), 0.0f)*/;
 	else if(textureBool == false)
-		diffuseLight = diffuseColor * max(dot(s, input.Nor), 0.0f);
+		diffuseLight = diffuseColor/* * max(dot(lightDir, input.Nor), 0.0f)*/;
 
-	float3 ambientLight = {0.2, 0.2, 0.2};
-
-	float3 specularLight = specularColor * pow(max(dot(r, v), 0.0f), shininess);
-
-	float4 color = float4((lightIntensity * ( ambientLight + diffuseLight /* + specularLight*/)), 1.0f);
+	/*float3 specularLight = specularColor * pow(max(dot(r, v), 0.0f), shininess);*/
+	
+	float4 color = float4((lightIntensity * ( /*ambientLight +*/ diffuseColor/* + specularLight*/)), 1.0f);
 
 
-	return color;
+	return -input.Nor;
 };
