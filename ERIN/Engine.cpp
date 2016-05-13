bdbb0c23850d65ef52de6a8a035f58b4afc0e90f
Taccoa <Mattias_M_93@hotmail.com>
2016-05-13 19:08:45 +0200

    Numbers

--- ERIN/Engine.cpp
+++ ERIN/Engine.cpp
@@ -297,6 +297,67 @@ Engine::Engine(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCommandLin
 		graphics->CreateTexture(customImport->meshes.at(34));
 		customImport->meshes.at(34).textureBool = true;
 
+		//Numbers
+		customImport->LoadCustomFormat("../BinaryDataNumber0.dat");
+		customImport->NewMesh();
+		graphics->CustomVertexBuffer(customImport->meshes.at(35));
+		graphics->CreateTexture(customImport->meshes.at(35));
+		customImport->meshes.at(35).textureBool = true;
+
+		customImport->LoadCustomFormat("../BinaryDataNumber1.dat");
+		customImport->NewMesh();
+		graphics->CustomVertexBuffer(customImport->meshes.at(36));
+		graphics->CreateTexture(customImport->meshes.at(36));
+		customImport->meshes.at(36).textureBool = true;
+
+		customImport->LoadCustomFormat("../BinaryDataNumber2.dat");
+		customImport->NewMesh();
+		graphics->CustomVertexBuffer(customImport->meshes.at(37));
+		graphics->CreateTexture(customImport->meshes.at(37));
+		customImport->meshes.at(37).textureBool = true;
+
+		customImport->LoadCustomFormat("../BinaryDataNumber3.dat");
+		customImport->NewMesh();
+		graphics->CustomVertexBuffer(customImport->meshes.at(38));
+		graphics->CreateTexture(customImport->meshes.at(38));
+		customImport->meshes.at(38).textureBool = true;
+
+		customImport->LoadCustomFormat("../BinaryDataNumber4.dat");
+		customImport->NewMesh();
+		graphics->CustomVertexBuffer(customImport->meshes.at(39));
+		graphics->CreateTexture(customImport->meshes.at(39));
+		customImport->meshes.at(39).textureBool = true;
+
+		customImport->LoadCustomFormat("../BinaryDataNumber5.dat");
+		customImport->NewMesh();
+		graphics->CustomVertexBuffer(customImport->meshes.at(40));
+		graphics->CreateTexture(customImport->meshes.at(40));
+		customImport->meshes.at(40).textureBool = true;
+
+		customImport->LoadCustomFormat("../BinaryDataNumber6.dat");
+		customImport->NewMesh();
+		graphics->CustomVertexBuffer(customImport->meshes.at(41));
+		graphics->CreateTexture(customImport->meshes.at(41));
+		customImport->meshes.at(41).textureBool = true;
+
+		customImport->LoadCustomFormat("../BinaryDataNumber7.dat");
+		customImport->NewMesh();
+		graphics->CustomVertexBuffer(customImport->meshes.at(42));
+		graphics->CreateTexture(customImport->meshes.at(42));
+		customImport->meshes.at(42).textureBool = true;
+
+		customImport->LoadCustomFormat("../BinaryDataNumber8.dat");
+		customImport->NewMesh();
+		graphics->CustomVertexBuffer(customImport->meshes.at(43));
+		graphics->CreateTexture(customImport->meshes.at(43));
+		customImport->meshes.at(43).textureBool = true;
+
+		customImport->LoadCustomFormat("../BinaryDataNumber9.dat");
+		customImport->NewMesh();
+		graphics->CustomVertexBuffer(customImport->meshes.at(44));
+		graphics->CreateTexture(customImport->meshes.at(44));
+		customImport->meshes.at(44).textureBool = true;
+
 		graphics->CreateConstantBuffer();
 		ShowWindow(wndHandle, nCommandShow);
 	}
