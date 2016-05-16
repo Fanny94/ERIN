6c26ffa4ba0441e1ca073d72bd9d4b84c0ff4520
BenjaminDannegard <benjamin.dannegard@hotmail.com>
2016-05-16 11:18:31 +0200

    added dmg sound

--- ERIN/Engine.cpp
+++ ERIN/Engine.cpp
@@ -976,6 +976,7 @@ void Engine::update(double deltaTimeMs)
 				if (player->getHpCooldown())
 				{
 					player->HP--;
+					sound->UseitSevereDmg();
 					enemyCount--;
 					Objectpool->enemies[i].setInUse(false);
 					player->setHpCooldown(false);
